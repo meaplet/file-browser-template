79e5d12438bf260cf49d01602846296aec35727e
Jen Bourey <jennifer.bourey@gmail.com>
2015-09-15 09:08:05 -0700

    Adding leftnav menu icons

--- app/src/files/Files.js
+++ app/src/files/Files.js
@@ -1,9 +1,16 @@
 const URL_AVATAR_ICONS = 'assets/svg/avatars.svg';
+const URL_ICON_DOWNLOAD    = 'assets/svg/download.svg';
 const URL_ICON_GRID    = 'assets/svg/grid.svg';
+const URL_ICON_FOLDER    = 'assets/svg/folder.svg';
 const URL_ICON_INFO    = 'assets/svg/info.svg';
 const URL_ICON_LIST    = 'assets/svg/list.svg';
 const URL_ICON_MENU    = 'assets/svg/menu.svg';
+const URL_ICON_PIN    = 'assets/svg/pin.svg';
+const URL_ICON_RECENT    = 'assets/svg/recent.svg';
 const URL_ICON_SEARCH    = 'assets/svg/search.svg';
+const URL_ICON_SHARE    = 'assets/svg/share.svg';
+const URL_ICON_STAR    = 'assets/svg/star.svg';
+const URL_ICON_UPLOAD    = 'assets/svg/upload.svg';
 
 // Load the custom app ES6 modules
 
@@ -31,11 +38,18 @@ let moduleName = angular
 
         $mdIconProvider
           .defaultIconSet( URL_AVATAR_ICONS, 128 )
+          .icon('download' ,URL_ICON_DOWNLOAD, 24)
+          .icon('folder' ,URL_ICON_FOLDER, 24)
           .icon('grid' ,URL_ICON_GRID, 24)
           .icon('info' ,URL_ICON_INFO, 24)
           .icon('list' ,URL_ICON_LIST, 24)
           .icon('menu' ,URL_ICON_MENU, 24)
-          .icon('search' ,URL_ICON_SEARCH, 24);
+          .icon('pin' ,URL_ICON_PIN, 24)
+          .icon('recent' ,URL_ICON_RECENT, 24)
+          .icon('search' ,URL_ICON_SEARCH, 24)
+          .icon('share' ,URL_ICON_SHARE, 24)
+          .icon('star' ,URL_ICON_STAR, 24)
+          .icon('upload' ,URL_ICON_UPLOAD, 24);
 
       })
       .name;
